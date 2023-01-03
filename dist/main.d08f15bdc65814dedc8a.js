e2733682610cf64fef6ff05f94dfdc1e38e00213
Evgeniy Doroshkevich <Doroshkeviches@yandex.ru>
2023-01-03 19:48:17 +0300

    feat: add files

---
+++ dist/main.d08f15bdc65814dedc8a.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./main.ts":
+/*!*****************!*\
+  !*** ./main.ts ***!
+  \*****************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (321:16)\\nFile was processed with these loaders:\\n * ../node_modules/ts-loader/index.js\\nYou may need an additional loader to handle the result of these loaders.\\n|             && (arrPrice.indexOf(+arr[2]) > -1 || arrPrice.length === 0)\\n|             && (arrStock.indexOf(+arr[3]) > -1 || arrStock.length === 0)\\n>             && ()) {\\n|             newItemsArray.push(item);\\n|             item.style.display = '';\");\n\n//# sourceURL=webpack:///./main.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./main.ts"]();
+/******/ 	
+/******/ })()
+;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './main.ts',// можно добавлять много точек входа 
+        ts: './typeS.ts'
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './main page.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCSSExtractPlugin({
+            filename: '[name].[contenthash].css'
+          })
+    ],
+    module: {
+        rules: [
+            {
+                test: /.css$/i,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader'],
+              },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+              },
+            
+           
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
+
+}
