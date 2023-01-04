81a3d17ba3cf7d00f376820b58e75fd867c45a22
RetiredHedgehog <lukashov9999@gmail.com>
2023-01-04 17:51:01 +0300

    feat: add price

--- src/cart/itemsBlock/item/item.ts
+++ src/cart/itemsBlock/item/item.ts
@@ -3,6 +3,7 @@ import { productItem } from "../../../interfaces/productsItem";
 import createImage from "./image/image";
 import createIndex from "./index";
 import { createInfo } from "./info/info";
+import createPrice from "./price/price";
 
 export default function createItem(
   item: productItem,
@@ -17,6 +18,7 @@ export default function createItem(
     createIndex(index),
     createImage(item),
     createInfo(item),
+    createPrice(item),
   );
 
   return wrapper;
---
+++ src/cart/itemsBlock/item/price/price.ts
@@ -0,0 +1,6 @@
+import { productItem } from "../../../../interfaces/productsItem";
+
+export default function createPrice(item: productItem) {
+  const wrapper = document.createElement('div');
+  return wrapper;
+}
