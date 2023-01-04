fe7d0856892e8520b557276a1b11a9c6238bbd88
RetiredHedgehog <lukashov9999@gmail.com>
2023-01-04 16:19:11 +0300

    feat: add breadcrumbs

--- src/product/product.ts
+++ src/product/product.ts
@@ -1,10 +1,13 @@
 import { productItem } from "../interfaces/productsItem";
+import createBreadcrumbs from "./breadcrumbs/breadcrumbs";
 
 export default function createProduct(item: productItem) {
   const wrapper = document.createElement('div');
   wrapper.classList.add('main-container');
 
-  wrapper.append();
+  wrapper.append(
+    createBreadcrumbs(item),
+  );
 
   return wrapper;
 }
