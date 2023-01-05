717ac90451ac7564923b2a8fabf608e4630f8677
RetiredHedgehog <lukashov9999@gmail.com>
2023-01-05 17:17:27 +0300

    refactor: change dom structure

--- src/cart/cart.ts
+++ src/cart/cart.ts
@@ -5,7 +5,18 @@ export default function createCart(cart: Cart) {
   const wrapper = document.createElement('div');
   wrapper.classList.add('main-container');
 
+  const wrapperTitle = document.createElement('div');
+  wrapperTitle.classList.add('main-container__title-container');
+
+  const title = document.createElement('h2');
+  title.classList.add('items-container__title');
+
+  title.innerText = cart.length > 0 ? 'Items in your cart' : 'Your cart is empty!';
+
+  wrapperTitle.append(title);
+
   wrapper.append(
+    wrapperTitle,
     createItemsBlock(cart),
   );
 
--- src/cart/itemsBlock/itemsBlock.ts
+++ src/cart/itemsBlock/itemsBlock.ts
@@ -1,20 +1,11 @@
 import Cart from "../../classes/Cart";
 import productItem from "../../interfaces/productsItem";
 import createItem from "./item/item";
+import createSummary from "./summary/summary";
 
 export default function createItemsBlock(cart: Cart) {
   const wrapper = document.createElement('div');
   wrapper.classList.add('main-container__items-container');
-
-  const wrapperTitle = document.createElement('div');
-  wrapperTitle.classList.add('items-container__title-container');
-
-  const title = document.createElement('h2');
-  title.classList.add('items-container__title');
-
-  title.innerText = cart.length > 0 ? 'Items in your cart' : 'Your cart is empty!';
-
-  wrapperTitle.append(title);
 
   const wrapperItems = document.createElement('div');
   wrapperItems.classList.add('items-container');
@@ -22,9 +13,9 @@ export default function createItemsBlock(cart: Cart) {
   wrapperItems.replaceChildren(
     ...cart.products.map((element: productItem, index: number) =>
       createItem(element, index, cart)
-    )
+    ),
   );
 
-  wrapper.append(wrapperTitle, wrapperItems);
+  wrapper.append(wrapperItems, createSummary(cart));
   return wrapper;
 }
